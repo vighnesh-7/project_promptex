beb616640f0e071fa33858dccb52f7d6100458d6
vighnesh-7 <vangarivighnesh@gmail.com>
2024-04-13 00:50:05 +0530

    implemented PWA

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,29 @@
 import '@styles/globals.css'
 import Footer from '@components/Footer';
 import DarkThemeToggle from '@components/DarkThemeToggle';
+// import  { Metadata, Viewport } from 'next';
+
 
 export const metadata ={
+    manifest: '/manifest.json',
     title:"Promptex",
     description : "Discover & Share AI Prompts",
     icons: {
         icon: "/assets/images/logo.svg",
     },
+    themeColor: '#e9e3dc',
 }
 
 const RootLayout = ({children}) => {
 return (
     <html className='dark:bg-stone-950 '>
+        <head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="theme-color" content="#ffffff" />
+        </head>
         <body  className='dark:bg-stone-950 w-full'>
             <DarkThemeToggle children={children} />
             <Footer/>
-
         </body>
     </html>
   )
--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")
+
 const nextConfig = {
   experimental: {
     appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
   images: {
     domains: ['lh3.googleusercontent.com'],
@@ -13,8 +19,17 @@ const nextConfig = {
       topLevelAwait: true,
     }
     return config
-  }
+  },
+  ...withPWA({
+    dest: 'public',
+    register: true,
+    skipWaiting: true,
+    // disable: process.env.NODE_ENV === 'development',
+    cacheOnFrontEndNav: true,
+    aggressiveFrontEndNavCaching: true,
+    disable: false,
+  })
 }
 
 
-module.exports = nextConfig
+module.exports = (nextConfig)
